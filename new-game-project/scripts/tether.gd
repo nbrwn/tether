a101827bc8f4dc8edf6b916cced00868655d22fa
Evan Roberts <evanrobertsruby@gmail.com>
2024-10-01 17:54:00 -0400

    generate_tether() now generates the tether without issue and the player is able to crudely attach the tether, though the tether does not exert force on the player to keep them in place

--- new-game-project/scripts/tether.gd
+++ new-game-project/scripts/tether.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
-@export var max_tether_length = 10
+@export var max_tether_length = 20
 var tether_segments = []
+var tether_segment = preload("res://scenes/TetherSegment.tscn")
 
 func _ready() -> void:
 	generate_tether()
@@ -12,18 +13,24 @@ func _process(delta: float) -> void:
 
 func generate_tether() -> void:
 	var previous_point = $TetherBase
-	var tether_segment = load("res://scenes/TetherSegment.tscn")
-	var offset = 0
+	var offset = 4
+	var step = 8
 	for i in max_tether_length:
+		#Create new tether_segment
 		var tether_segment_instance = tether_segment.instantiate()
-		tether_segment_instance.get_node("SegmentPinJoint").node_a = previous_point.get_path()
-		add_child(tether_segment_instance)
-		offset += 10
-		tether_segment_instance.transform = Transform2D(0.0, Vector2(offset, 0))
-		tether_segment_instance.rotation_degrees = 90
 		tether_segments.append(tether_segment_instance)
+		add_child(tether_segment_instance)
+		
+		#Move, Rotate, Pin new tether_segment
+		tether_segment_instance.rotation_degrees = -90
+		tether_segment_instance.position = Vector2(offset, 0)
+		tether_segment_instance.get_node("SegmentPinJoint").node_a = previous_point.get_path()
+		
+		#Iterate
 		previous_point = tether_segment_instance
-	$TetherHead.transform = Transform2D(0.0, Vector2(offset + 10, 0))
+		offset += step
+	#Attach TetherHead to end of tether
+	$TetherHead.transform = Transform2D(0.0, Vector2(offset, 0))
 	$TetherHead/HeadToSegmentPinJoint.node_a = previous_point.get_path()
 
 	
